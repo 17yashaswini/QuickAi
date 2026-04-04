1fee7705251f7dd8678318eb9f1bb333446322f8
Yashaswini <yashu77saana@gmail.com>
2026-04-04 20:39:42 +0530

    hardcode api url

--- quickai/client/src/api/axios.js
+++ quickai/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://quickai-server-qs6h.onrender.com',
+ baseURL: 'https://quickai-server-qs6h.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
